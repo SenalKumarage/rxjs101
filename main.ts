701438a8eebe5433996e667cf801bf4ee0511cd7
Senal Kumarage <senalkumarage@gmail.com>
2018-03-13 14:17:18 +0800

    Working with Observables

--- main.ts
+++ main.ts
@@ -1,25 +1,66 @@
-import {Observable} from "rxjs";
+import { Observable } from "rxjs";
 
-let circle = document.getElementById("circle");
-let source = Observable.fromEvent(document, "mousemove")
-                       .map((e: MouseEvent) => {
-                           return {
-                               x: e.clientX,
-                               y: e.clientY
-                           }
-                       })
-                       .filter(value => value.x < 500)
-                       .delay(300);
-// .map(n => n*2)
-//   .filter(n => n > 4);
+let output = document.getElementById("output");
+let button = document.getElementById("button");
 
-function onNext(value) {
-    circle.style.left = value.x;
-    circle.style.top = value.y;
+let click = Observable.fromEvent(document, "click");
+
+function load(url: string) {
+    return Observable.create(observer => {
+        let xhr = new XMLHttpRequest();
+
+        xhr.addEventListener("load", () => {
+
+            if(xhr.status === 200) {
+                let data = JSON.parse(xhr.responseText);
+                observer.next(data);
+                observer.complete();
+            } else {
+                observer.error(xhr.statusText);
+            }
+
+        });
+
+        xhr.open("GET", url);
+        xhr.send();
+    }).retryWhen(retryStrategy({attempts: 4, delay: 1500}));
 }
+
+function loadWithFetch(url: string) {
+    return Observable.defer(() => {
+        return Observable.fromPromise(fetch(url).then(r => r.json()))
+    });
+}
+
+
+function retryStrategy({attempts = 4, delay = 1000}) {
+    return (errors) => {
+        return errors
+            .scan((acc, value)=>{
+                console.log(acc, value);
+                return ++acc;
+            }, 0)
+            .takeWhile(acc => acc < attempts)
+            .delay(delay);
+    }
+}
+
+function renderMovies(movies) {
+    movies.forEach(m => {
+        let div = document.createElement("div");
+        div.innerText = m.title;
+        output.appendChild(div);
+    });
+}
+
+loadWithFetch("movies.json");
+
 // Observer creation 2
-source.subscribe(
-    onNext,
-    e => console.log(`error: ${e}`),
-    () => console.log(`complete`)
-);
+click.flatMap(e => loadWithFetch("movies.json"))
+    .subscribe(
+        renderMovies,
+        e => console.log(`error: ${e}`),
+        () => console.log(`complete`)
+    );
+
+
