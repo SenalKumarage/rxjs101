587d1b6be0f4a59c976272888c06c3e5eb044fe7
Senal Kumarage <senalkumarage@gmail.com>
2018-03-13 11:19:04 +0800

    Moving circle

--- main.ts
+++ main.ts
@@ -1,65 +1,25 @@
 import {Observable} from "rxjs";
-// import {Observable} from "rxjs/Observable";
-// import "rxjs/add/operator/map";
-// import "rxjs/add/operator/filter";
- 
 
+let circle = document.getElementById("circle");
+let source = Observable.fromEvent(document, "mousemove")
+                       .map((e: MouseEvent) => {
+                           return {
+                               x: e.clientX,
+                               y: e.clientY
+                           }
+                       })
+                       .filter(value => value.x < 500)
+                       .delay(300);
+// .map(n => n*2)
+//   .filter(n => n > 4);
 
-let numbers = [1,5,10];
-// let source = Observable.from(numbers);
-
-// let source = Observable.create(observer => {
-
-//     for(let n of numbers) {
-//         observer.next(n);
-
-//         if(n==5) observer.error(n);
-//     }
-
-//     observer.complete();
-
-// });
-
-let source = Observable.create(observer => {
-
-    let index = 0;
-
-    let produceValue = () => {
-
-        observer.next(numbers[index++]);
-
-        if(index < numbers.length) {
-            setTimeout(produceValue, 2000);
-        } else {
-            observer.complete();
-        }
-    }
-
-    produceValue();
-
-}).map(n => n*2)
-  .filter(n => n > 4);
-
-// Observer creation 1
-// class MyObserver implements Observer<number> {
-//     next(value) {
-//         console.log(`value: ${value}`);
-//     }
-
-//     error(e) {
-//         console.log(`error: ${e}`);
-//     }
-
-//     complete() {
-//         console.log("complete");
-//     }
-// }
-
-// source.subscribe(new MyObserver());
-
+function onNext(value) {
+    circle.style.left = value.x;
+    circle.style.top = value.y;
+}
 // Observer creation 2
 source.subscribe(
-    value => console.log(`value: ${value}`),
+    onNext,
     e => console.log(`error: ${e}`),
     () => console.log(`complete`)
 );
